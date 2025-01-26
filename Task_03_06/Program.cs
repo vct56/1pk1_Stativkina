ff2b60539f6860fa2975566a25316cc93b2327cf
kt958539@gmail.com <kt958539@gmail.com>
2025-01-26 23:08:07 +0300

    выполнено задание 6

---
+++ Task_03_06/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_03_06
+{
+    internal class Program
+    {
+          static void Main(string[] args)
+            {
+                double startX = -4.0;
+                double endX = 4.0;
+                double step = 0.5;
+
+                // Выводим заголовок таблицы
+                Console.WriteLine("Таблица значений функции y = |x|");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("x\t|\ty");
+                Console.WriteLine("-----------------------------------");
+
+                // Вычисляем и выводим значения функции
+                for (double x = startX; x <= endX; x += step)
+                {
+                    double y = Math.Abs(x);
+                    Console.WriteLine($"{x}\t|\t{y}");
+                }
+
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Нажмите любую клавишу для выхода.");
+                Console.ReadKey();
+            }
+        }
+    }
