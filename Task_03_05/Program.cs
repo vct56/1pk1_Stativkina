fced8a083749211779dcfcd3c854e5c58cb10115
kt958539@gmail.com <kt958539@gmail.com>
2025-01-26 23:01:23 +0300

    выполено задание 5

---
+++ Task_03_05/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_03_05
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Ввод диапазона температур
+            Console.WriteLine("Введите начальную температуру в градусах Цельсия: ");
+            double startCelsius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите конечную температуру в градусах Цельсия: ");
+            double endCelsius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите шаг изменения температуры: ");
+            double step = Convert.ToDouble(Console.ReadLine());
+
+            // Вывод заголовка таблицы
+            Console.WriteLine("\nТемпература (°C) | Температура (°F)");
+            Console.WriteLine("------------------ |------------------");
+
+            // Вывод таблицы
+            for (double celsius = startCelsius; celsius <= endCelsius; celsius += step)
+            {
+                double fahrenheit = celsius * 1.8 + 32;
+                Console.WriteLine($"{celsius,-18} | {fahrenheit,-18}");
+            }
+        }
+    }
+}
