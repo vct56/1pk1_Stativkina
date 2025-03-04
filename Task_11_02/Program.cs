e4d67cefa68e6a86a971682dae3b51b1584e13a9
kt958539@gmail.com <kt958539@gmail.com>
2025-03-04 19:22:26 +0300

    выполнено задание 3

---
+++ Task_11_02/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_11_02
+{
+    internal class Program
+    {
+        //Передача по ссылке (ref): Напишите метод,
+        //который принимает два целых числа по ссылке
+        //и меняет их местами.Проверьте,
+        //изменились ли значения переменных вне метода
+
+
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            Console.WriteLine($"Перед вызовом метода: a = {a}, b = {b}");
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"После вызова метода: a = {a}, b = {b}");
+        }
+    }
+    
+}
---
+++ Task_11_03/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_11_03
+{
+    internal class Program
+    {
+        //Выходной параметр (out): Напишите метод на с #,
+        //который принимает строки и возвращает через выходной параметр
+        //количество гласных и согласных букв в этой строк
+         
+        static void Main()
+        {
+            string input = "лоаиркорирууоооКЛилФ";
+            CountLetters(input, out int vowelsCount, out int consonantsCount);
+            Console.WriteLine($"Гласные: {vowelsCount}, Согласные: {consonantsCount}");
+        }
+
+        static void CountLetters(string str, out int vowelsCount, out int consonantsCount)
+        {
+            vowelsCount = 0;
+            consonantsCount = 0;
+            string vowels = "аеиоуюэёАЕИОУЮЭЁ";
+
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (vowels.Contains(c))
+                        vowelsCount++;
+                    else
+                        consonantsCount++;
+                }
+            }
+        }
+    }
+
+}
+
