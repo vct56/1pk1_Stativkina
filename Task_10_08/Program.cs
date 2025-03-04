8784e61bd952f26ba0abfc5f2af50b1f7dddf1be
kt958539@gmail.com <kt958539@gmail.com>
2025-03-04 18:55:21 +0300

    выполнено задание 1

---
+++ Task_10_08/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_10_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Task_11_01/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_11_01
+{
+    internal class Program
+    {
+
+        //Напишите метод, который принимает два целых числа
+        //и меняет их местами.
+        
+        public class SwapNumbers
+        {
+            public static void Swap(ref int x, ref int y)
+            {
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+
+            public static void Main(string[] args)
+            {
+                int a = 66;
+                int b = 77;
+
+                Console.WriteLine($"до изменения : a = {a}, b = {b}"); // Выведет: до изменения : a = 10, b = 20
+
+                Swap(ref a, ref b); 
+
+                Console.WriteLine($"после измениния : a = {a}, b = {b}"); // Выведет: росле изменения : a = 20, b = 10
+            }
+        }
+    }
+}
+
+
+
+
+
