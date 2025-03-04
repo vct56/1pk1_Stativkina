812b2b352de9de86a83f4dc0f81281041ca1e5d5
kt958539@gmail.com <kt958539@gmail.com>
2025-03-04 20:23:00 +0300

    выполнено задание 7

---
+++ Task_11_07/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task_11_07
+{
+    internal class Program
+    {
+        //Передача массива по ссылке (ref):
+        //Напишите метод, который принимает массив целых чисел по ссылке и изменяет его элементы,
+        //увеличивая каждый на 1. Проверьте, изменился ли оригинальный массив вне метода.
+
+        public static void Increment(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]++; 
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 3, 4,};
+
+            Console.WriteLine("Оригинальный массив:");
+            PrintArray(myArray); // Вывод оригинального массива
+
+            Increment(ref myArray); // Вызов метода с передачей по ссылке
+
+            Console.WriteLine("\nМассив после IncrementElements:");
+            PrintArray(myArray); 
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (var a in arr)
+            {
+                Console.Write($"{a} "); 
+            }
+            Console.WriteLine(); 
+        }
+    }
+
+}
+  
